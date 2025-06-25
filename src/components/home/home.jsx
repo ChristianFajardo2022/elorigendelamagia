0167aa1145a1d8cd92efc287e6813ec75d09c881
Christian Camilo Fajardo Hernandez <chfajard@gmail.com>
2025-06-25 16:56:15 -0500

    categorias avanzado

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -51,9 +51,9 @@ export const Home = () => {
       />
       <SectionMakingof data={data} handlePlay={handlePlay} />
       <EntregasSection dataList={Data} handlePlay={handlePlay} />
-<p>Un mejor mundo para nuestras futuras generaciones, esa será nuestra entrega. Para lograrlo decidimos dar el primer paso, llevamos a cientos de pescadores de plástico a las playas de Colombia con la misión de recolectar el plástico que llega hasta allí y recuperar estos ecosistemas.
-Ecosistemas que están siendo alterados y afectados a tal punto que nuestras especies obligadas a adaptarse a estos entornos que se están convirtiendo en una pesadilla para su supervivencia. Por esta razón, estamos invitando a todas las personas a hacer un uso adecuado del plástico, un uso consciente que no ponga en riesgo la vida de nuestras especies, o a usar alternativas que no representen un peligro para los ecosistemas.
-Conoce más sobre esta campaña en mundosinplastico.com </p>
+    <p>¡Solo pasamos a recordarles que esta entrega ya empezó!</p>
+    <p>Inter Rapidísimo. Orgulloso Patrocinador Oficial Sudamericano Catar 2022™ y de nuestras valientes guerreras.</p>
+    
     </>
   );
 };
